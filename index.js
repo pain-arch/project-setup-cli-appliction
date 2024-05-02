f91b0449e3d0ff90d14560e254550344327f7a49
pain-arch <himulol123@gmail.com>
2024-05-03 04:57:11 +0600

    feat: Create project structure and files programmatically

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ const projectStructure = {
     "public": ['index.html', 'style.css']
 };
 
+Object.entries(projectStructure).forEach(([dir, files]) => {
+    
+    // Create directory and files
+    fs.mkdirSync(dir, { recursive: true });
+    files.forEach(file => fs.writeFileSync(`${dir}/${file}`, ''));
+});
+
+console.log('Project structure created successfully!');
