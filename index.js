d6bf6f18adeb7912a8dae61f838e4434e325615d
pain-arch <himulol123@gmail.com>
2024-05-03 04:56:48 +0600

    chore: Add initial project structure

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+
+// Require Node.js modules
+const fs = require('fs');
+
+// Require third-party modules
+const projectStructure = {
+    "src": ['index.js'],
+    "public": ['index.html', 'style.css']
+};
+
